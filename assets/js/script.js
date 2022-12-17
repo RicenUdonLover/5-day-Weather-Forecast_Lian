7b0a967fbac3300e13cce8c5f2a6c8d82328d43f
Lian Liu <riceudon@gmail.com>
2022-12-17 12:44:07 -0800

    Link the search text and get geocode function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,50 @@
 const options = {
-    types: ["(cities)"]
+  types: ["(cities)"]
 }
+
 const presentDate = dayjs().format('dddd, MMM DD, YYYY')
 const geocoder = new google.maps.Geocoder();
 const inputLocationEL = document.querySelector('#location')
 const subTitleEl = $('.subtitle')
-
-
+const searchBtnEl = $('#search-btn')
 
 //Functions--------------------------------------------------------
 showDate = dateContainer => dateContainer.text(`Today is ${presentDate}`)
 addAutoComplete = (input, options) => new google.maps.places.Autocomplete(input, options)
+getText = (event)  => {
+  event.preventDefault();
+  var inputLocationText = inputLocationEL.value.trim()
+  console.log(inputLocationText)
+  getGeocode(inputLocationText)
+}
 
 
-
-
-// Next, make a request to the geocode method, passing in the city name
-// and a callback function to handle the response
-geocoder.geocode({ address: 'Seattle' }, (results, status) => {
-  // If the request was successful
-  if (status === google.maps.GeocoderStatus.OK) {
-    // Get the first result (which should be the most accurate)
-    const result = results[0];
-    // Get the latitude and longitude of the city
-    const lat = result.geometry.location.lat();
-    const lng = result.geometry.location.lng();
-
-    // Print the latitude and longitude to the console
-    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-  } else {
-    // If the request was not successful, print the error to the console
-    console.error(`Geocoding error: ${status}`);
+getGeocode = (address, callback) => {
+  if (address) {
+    console.log(address)
+    // Next, make a request to the geocode method, passing in the city name
+    // and a callback function to handle the response
+    geocoder.geocode({ address: address }, (results, status) => {
+      // If the request was successful
+      if (status === google.maps.GeocoderStatus.OK) {
+        // Get the first result (which should be the most accurate)
+        const result = results[0];
+        // Get the latitude and longitude of the city
+        const lat = result.geometry.location.lat();
+        const lng = result.geometry.location.lng();
+        // Print the latitude and longitude to the console
+        console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+      } else {
+        // If the request was not successful, print the error to the console
+        console.error(`Geocoding error: ${status}`);
+      }
+    });
   }
-});
+}
+
+get
+
 //Excute functions
 showDate(subTitleEl)
 addAutoComplete(inputLocationEL, options)
+searchBtnEl.click(getText)
